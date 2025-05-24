2bfce6362ff4950851cd2bfb0321285f5a493845
Benjamin Timm <benjamintimm21@gmail.com>
2025-05-24 11:23:58 +0200

    reedit parallax script
    not it doesnt flicker anymore? doesnt know why it did that

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -11,24 +11,27 @@ public class ParallaxEffect : MonoBehaviour
 
 
     void Start()
-    {
-     _startPos = transform.position.x;
-     _length = GetComponent<SpriteRenderer>().bounds.size.x;
+    { 
+        _length = GetComponent<SpriteRenderer>().bounds.size.x; 
+        _startPos = transform.position.x;
+     
     }
 
     void Update()
     {
-        float temp = (_camera.transform.position.x * (1 - _parallaxEffect));
-        float distance = (_camera.transform.position.x * _parallaxEffect);
-        transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
+        float temp = (_camera.transform.position.x * (1- _parallaxEffect));
+       float distance = (_camera.transform.position.x * _parallaxEffect);
+       transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
 
-        if (temp > _startPos * _length)
-        {
-            _startPos += _length;
-        } else if (temp < _startPos + _length)
-        {
-            _startPos -= _length;
-        }
+       if (temp > _startPos + _length)
+       {
+           _startPos += _length;
+       }
+       else if (temp < _startPos - _length)
+       {
+           _startPos -= _length;
+       }
+       
     }
 }
 
